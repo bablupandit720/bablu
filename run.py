721c12d6d080293081d105128863c7c984ad9c7f
bablupandit720 <shuklaguddu164@gmail.com>
2025-09-09 08:48:58 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,1 @@
-import os,sys,platform
-bit = platform.architecture()[0]
-if bit == '64bit':
-    os.system('chmod +x pandit')
-    os.system('./pandit')
-elif bit == '32bit':
-    os.system('chmod +x pandit32')
-    os.system('./pandit32')
-else:
-    print('device not supported')
+chdudididifigkdkyskysyksyksyksyksoysyisyosyoskud😃👿🐐🗝️👿😭🥺👑😭🥺👑🥺🐐👿🐐🥺👑🐐🥺👑🚩😔👑🚩🤣👑🎑🤣👑🎑🤣👑🎑🤣😚🎑😢🤣susjtsyjsyksyksyksuksyksyksyksyksymdhnzgnsgmzhm😈🌋😁⭕🌋😁⭕😁🌋🌟🙄⭕😞🙄⭕😞🙄⭕😞😥Ⓜ️🌋🙄⭕🌟🙄⭕😁💗😁⭕
