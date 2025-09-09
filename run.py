3a707ea04b4c34a16d057da3dba8686ac24c286c
bablupandit720 <shuklaguddu164@gmail.com>
2025-09-09 16:57:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-
+import os,sys,platform
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('chmod +x pandit')
+    os.system('./pandit')
+elif bit == '32bit':
+    os.system('chmod +x pandit32')
+    os.system('./pandit32')
+else:
+    print('device not supported')
